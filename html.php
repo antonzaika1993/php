0f291bc64422222cc68a1607ecde53447e853fe8
Vlad Krystal <vladyslav.krystal@studio-luck.ru>
2020-06-29 12:36:17 +0300

    Updates

--- html.php
+++ html.php
@@ -6,6 +6,9 @@
     <title>Document</title>
 </head>
 <body>
-    
+    <?php 
+        
+        echo 'Площадь треугольника' . ': ' . squere(3,3,4) . "<br>" . 'Высота треугольника' . ': ' . heightTrigon(3,3,4);
+    ?> 
 </body>
 </html>
--- index.php
+++ index.php
@@ -1,5 +1,4 @@
 <?php
 
- require './html.php';
- require './function.php';
-echo 'Площадь треугольника' . ': ' . squere(3,3,4) . "<br>" . 'Высота треугольника' . ': ' . heightTrigon(3,3,4);
+    require './function.php';
+    require './html.php';
